b5b5addf3e9618491025d3e83497edc07c1617de
Hyunbin Kim <147928172+wien0128@users.noreply.github.com>
2024-05-29 15:29:38 +0900

    Create BJ_24444_BFS.cpp

---
+++ BaekJoon/BJ_24444_BFS.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+// 백준 24444 BFS
+// 기초적인 BFS 구현 문제
+
+class Graph {
+private:
+    vector<vector<int>> adj_list;   // 간선 리스트
+    queue<int> q;                   // 인접 정점 큐
+    vector<int> visited_order;      // 정점 방문 순서
+    int current_order;              // 현재 방문 순서
+public:
+    // 생성자, 1-based-indexing
+    Graph(int n) : adj_list(n + 1), visited_order(n + 1, 0), current_order(1){}
+
+    // 간선 추가
+    void add_edge(int u, int v) {
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    // 인접 리스트 오름차순 정렬
+    void prepare() {
+        for (auto& neighbors : adj_list)
+        {
+            ranges::sort(neighbors);
+        }
+    }
+
+    // BFS
+    void bfs(int node) {
+
+        visited_order[node] = current_order++;   // 시작 노드는 항상 첫 번째 방문
+        q.push(node);
+        
+        while (!q.empty())
+        {
+            int current = q.front(); // 현재 정점
+            q.pop();
+
+            // 현재 정점의 모든 인접 정점 순회
+            for (auto& neighbors : adj_list[current])
+            {
+                // 이전에 방문했는가?
+                if (!visited_order[neighbors])
+                {
+                    q.push(neighbors); // 인접 정점 push
+                    visited_order[neighbors] = current_order++; // 방문 순서 기록
+                }
+            }
+        }
+    }
+
+    // 출력
+    void print_visit_order() const
+    {
+        for (size_t i = 1; i < visited_order.size(); ++i)
+        {
+            cout << visited_order[i] << "\n";
+        }
+    }
+};
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    Graph graph(n);
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        graph.add_edge(u, v);
+    }
+
+    graph.prepare();
+
+    graph.bfs(r);
+    graph.print_visit_order();
+    
+    return 0;
+}
