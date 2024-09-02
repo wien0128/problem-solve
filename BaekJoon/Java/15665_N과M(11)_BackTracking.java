dba71062e4ace1c94baf7272fc1be18350dda751
Hyunbin Kim <147928172+wien0128@users.noreply.github.com>
2024-09-02 14:33:47 +0900

    Create 15665_N과M(11)_BackTracking.java

---
+++ BaekJoon/Java/15665_N과M(11)_BackTracking.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// N과M(11) 15665 BackTracking
+// O( M * N^M )
+
+
+public class Main {
+    static int n, m;
+    static int[] nums, seq;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        seq = new int[m];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; ++i) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        DFS(0);
+
+        System.out.println(sb);
+    }
+
+    private static void DFS(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; ++i) {
+                sb.append(seq[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+        for (int i = 0; i < n; ++i) {
+            if (prev == nums[i]) continue;
+
+            prev = nums[i];
+            seq[depth] = nums[i];
+            DFS(depth + 1);
+        }
+    }
+}
